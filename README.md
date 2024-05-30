1107ce31f6ab3e2a8e2a56657da6c0af0f5cd4bd
Sl4y3r-07 <ayush_g@ee.iitr.ac.in>
2024-05-30 20:39:44 +0530

    piping

--- README.md
+++ README.md
@@ -1,3 +1,9 @@
 # my-shell
 A custom shell implementation written in C++
 
+# Features
+
+- Basic Command Execution
+- Piping (`|`) support
+- Command history
+
--- main.cpp
+++ main.cpp
@@ -6,11 +6,14 @@
 
 using namespace std;
 
+vector<string> history;
+string line;
+
 string read_line()
 { 
-    string line;
-    getline(cin, line);
-    return line;   
+    string line_;
+    getline(cin, line_);
+    return line_;   
 }
 
 vector<string> parsing_string(const string &line)
@@ -34,7 +37,28 @@ vector<string> parsing_string(const string &line)
     return args;
 }
 
-int execute_command(const vector<string> &args)
+vector<string> split_pipe(const string &line)
+{  vector<string> commands;
+   string command="";
+    for(int i=0;i<line.length();i++)
+    {
+      if(line[i]=='|')
+      { 
+        
+        commands.push_back(command);
+        command.clear();
+        
+      }
+      else{
+        command+=line[i];
+      }
+ }
+ commands.push_back(command);
+ return commands;
+}
+
+
+int without_pipe_execute_command(const vector<string> &args)
 {
   int status;
 
@@ -58,6 +82,15 @@ int execute_command(const vector<string> &args)
   
   if(args[0]=="exit")
   {
+    return 0;
+  }
+
+  if(args[0]=="history")
+  {
+    for(int i=0;i<history.size();i++)
+    {
+      cout<<i+1<<" "<<history[i]<<"\n";
+    }
     return 0;
   }
 
@@ -91,15 +124,119 @@ int execute_command(const vector<string> &args)
 
 }
 
+
+int with_pipe_execute_command(const vector<string> &commands) // with pipe
+{   
+    int status;
+
+    // first execute the first command and then redirect the output of the first command to second command
+    
+    int pipes_count= commands.size()-1;
+    int pipefds[pipes_count*2]; // twice of the pipes_count, because two file descriptors are required - one for reading file, other for writing files
+     
+    for(int i=0;i<pipes_count;i++)
+    {
+        if(pipe(pipefds+i*2)==-1)   // pipe syscall initializes two consecutive elements of the pipefds array for each pipe it creates
+        {
+            perror("pipe");
+            return 1;
+        }
+    }
+
+    for(int i=0;i<commands.size();i++)
+    {
+        pid_t pid= fork();
+        if(pid==0)  // child process
+        {
+          // redirect i/p from the previous pipe
+           if (i > 0) {
+                if (dup2(pipefds[(i - 1) * 2], 0) == -1) {
+                    perror("dup2");
+                    exit(EXIT_FAILURE);
+                }
+            }
+
+            // redirect output to next pipe
+            if (i < commands.size() - 1) {
+                if (dup2(pipefds[i * 2 + 1], 1) == -1) {
+                    perror("dup2");
+                    exit(EXIT_FAILURE);
+                }
+            }
+
+            
+            for (int j = 0; j < 2 * pipes_count; j++) {  // Close all pipe file descriptors
+                close(pipefds[j]);
+            }
+
+            vector<string> args = parsing_string(commands[i]);  // same normal logic
+            vector<char *> cargs;
+            for (const auto &arg : args) {
+                cargs.push_back(const_cast<char *>(arg.c_str()));
+            }
+            cargs.push_back(nullptr);
+
+            if (execvp(cargs[0], cargs.data()) == -1) {
+                perror("exec");
+                exit(EXIT_FAILURE);
+            }
+        } else if (pid < 0) {
+            perror("fork");   // Error in forking
+            return 1;
+        }
+          
+    }
+
+      for (int i = 0; i < 2 * pipes_count; i++) {
+        close(pipefds[i]);
+    }
+
+    // Wait for all child processes to complete
+    for (size_t i = 0; i < commands.size(); i++) {
+        do {
+            waitpid(-1, &status, WUNTRACED);
+        } while (!WIFEXITED(status) && !WIFSIGNALED(status));
+    }
+    return status;
+}
+
+int execute_command(const vector<string> &args)
+{
+     if(args[0]=="history")
+  {
+    for(int i=0;i<history.size();i++)
+    {
+      cout<<i+1<<" "<<history[i]<<"\n";
+    }
+    return 0;
+  }
+
+    
+    vector<string> commands = split_pipe(line);
+
+    if (commands.size() > 1) {
+        return with_pipe_execute_command(commands);
+    } else {
+        return without_pipe_execute_command(args);
+    }
+
+}
+
 void shell_loop()
 {  
-  string line;
+  
   vector<string> args;
+  
   int status;
   do
   {
     cout<<"my-sh > ";
     line=read_line();
+    if(line.empty())
+    {
+        continue;
+    }
+    history.push_back(line);  // to save the history
     args=parsing_string(line);
     status= execute_command(args); 
     // cout<<status<<"\n";
